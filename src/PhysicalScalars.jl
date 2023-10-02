3eb385f0c00df534fb1f8d843a552a6c953c3aef
Alan D. Freed <alan.d.freed@protonmail.com>
2023-10-02 07:35:22 -0400

    Update PhysicalScalars.jl
    Fixed bug in square root function of a scalar field.

--- src/PhysicalScalars.jl
+++ src/PhysicalScalars.jl
@@ -627,9 +627,7 @@ function Base.:(floor)(y::PhysicalScalar)::PhysicalScalar
 end
 
 function Base.:(sqrt)(y::PhysicalScalar)::PhysicalScalar
-    if isDimensionless(y)
-        units = Dimensionless()
-    elseif isSI(y)
+    if isSI(y)
         if ((y.units.length%2 == 0) &&
             (y.units.mass%2 == 0) &&
             (y.units.amount_of_substance%2 == 0) &&
